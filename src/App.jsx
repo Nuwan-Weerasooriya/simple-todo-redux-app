2e17797dc5221c050fbbaf6c93e466fc7c478982
Nuize01-dev <nuwanweerasooriyabgn@gmail.com>
2026-02-25 20:12:12 +0530

    Create redux task store functionalities and config redux store.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import {useState} from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { storeTask } from "./utilities/state/taskSlice.js";
 
 const App = () => {
-    const [task, setTask] = useState('')
+  const [task, setTask] = useState("");
+  const dispatch = useDispatch();
 
-    const handleInput = (event) => {
-        setTask(event.target.value)
-    }
+  const handleInput = (event) => {
+    setTask(event.target.value);
+  };
+
+  const addNewTask = () => {
+    dispatch(storeTask(task));
+  };
 
   return (
     <>
@@ -25,14 +32,15 @@ const App = () => {
         />
         <button
           type="button"
+          onClick={addNewTask}
           className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
         >
           Add New Task
         </button>
       </div>
-        <div>
-            {task}
-        </div>
+      {/*<div>*/}
+      {/*    {task}*/}
+      {/*</div>*/}
     </>
   );
 };
